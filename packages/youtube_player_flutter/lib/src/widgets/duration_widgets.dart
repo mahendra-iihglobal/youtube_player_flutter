fe6477853a9ededd94a857b306e0df56c7ba9e3e
mahendra-iihglobal <73980629+mahendra-iihglobal@users.noreply.github.com>
2024-07-04 18:39:29 +0530

    video duration widget added

--- packages/youtube_player_flutter/lib/src/widgets/duration_widgets.dart
+++ packages/youtube_player_flutter/lib/src/widgets/duration_widgets.dart
@@ -7,6 +7,64 @@ import 'package:flutter/material.dart';
 import '../utils/duration_formatter.dart';
 import '../utils/youtube_player_controller.dart';
 
+class VideoDuration extends StatefulWidget {
+  /// Overrides the default [YoutubePlayerController].
+  final YoutubePlayerController? controller;
+  final TextStyle? textStyle;
+
+  /// Creates [CurrentPosition] widget.
+  const VideoDuration({super.key, this.controller, this.textStyle});
+
+  @override
+  State<VideoDuration> createState() => _VideoDurationState();
+}
+
+class _VideoDurationState extends State<VideoDuration> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final controller = YoutubePlayerController.of(context);
+    if (controller == null) {
+      assert(
+        widget.controller != null,
+        '\n\nNo controller could be found in the provided context.\n\n'
+        'Try passing the controller explicitly.',
+      );
+      _controller = widget.controller!;
+    } else {
+      _controller = controller;
+    }
+    _controller.removeListener(listener);
+    _controller.addListener(listener);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(listener);
+    super.dispose();
+  }
+
+  void listener() {
+    if (mounted) setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      durationFormatter(
+        _controller.metadata.duration.inMilliseconds,
+      ),
+      style: widget.textStyle ??
+          const TextStyle(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+    );
+  }
+}
+
 /// A widget which displays the current position of the video.
 class CurrentPosition extends StatefulWidget {
   /// Overrides the default [YoutubePlayerController].
